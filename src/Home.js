1ad42f2dc23caac796752b64a708c3a627f723f2
Emmanuel Balogun Oluwadamilare <emmanx25@gmail.com>
2023-05-23 23:33:33 +0100

    new commit

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,14 @@
-import { useState, useEffect  } from "react"
 import BlogList from "./Blog";
+import useFetch from "./useFetch";
 
 const Home = () => {
-
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'alex', id: 3 }
-    ])
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
-
-    useEffect(() => {
-        console.log('ran useeffect')
-        console.log(blogs)
-    })
-
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
+    
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete}/>
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title='All Blogs!'/>}
         </div>    
     );
 }
